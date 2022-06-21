850ec51a195e3976a89df5b8b10499a1173fb959
thanh.pnguyen <thanh.pnguyen@GXHW2D3>
2022-06-21 20:21:03 +0700

    https://github.com/lana199199/SecondRepo02

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>da683cad-c088-4186-8b94-0416f9e5176b</testSuiteGuid>
+</TestSuiteEntity>
